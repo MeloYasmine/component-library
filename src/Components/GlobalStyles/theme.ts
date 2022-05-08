48eebd30d60a792f36fe24e56faaadc30f798bf8
Yasmine Melo <yasmine.melo@hotmail.com>
2022-05-07 21:12:01 -0300

    feat: :sparkles: add resources for global styling

---
+++ src/Components/GlobalStyles/theme.ts
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#9400D3',
+    },
+  },
+});
+
+export default theme;
+
---
+++ src/Components/GlobalStyles/themeDeclare.d.ts
@@ -0,0 +1,26 @@
+/* eslint-disable */
+// import { Palette, PaletteColor } from '@material-ui/core/styles/createPalette';
+// import { Shadows } from '@material-ui/core/styles/shadows';
+import { Theme as MaterialTheme } from '@mui/material'
+import 'styled-components';
+
+declare module '@material-ui/core/styles/createPalette' {
+
+  interface Palette {
+    cpBackgrounds: Palette['primary'];
+    processing: Palette['primary'];
+    approval: Palette['primary'];
+    actionNeeded: Palette['primary'];
+    accentColors: AccentPaletteColor;
+    textOn: TextColor;
+  }
+};
+
+declare module '@mui/material/styles' {
+  interface Theme extends CustomTheme {}
+  interface ThemeOptions extends CustomTheme {}
+}
+
+declare module 'styled-components' {
+  export interface DefaultTheme extends Theme {}
+}
