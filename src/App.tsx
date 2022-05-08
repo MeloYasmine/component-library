e76231d641ee68345ed958aee331bc1bc62e244c
Yasmine Melo <yasmine.melo@hotmail.com>
2022-05-08 01:42:53 -0300

    feat: :sparkles: add Button Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button } from './Components/Button';
+import { StyledEngineProvider } from '@mui/material';
+import { ThemeProvider } from './Components/GlobalStyles/ThemeProvider';
+import { StyledDiv } from './styles';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <StyledEngineProvider injectFirst>
+        <ThemeProvider >
+          <Button color='primary' variant='contained'>BOTÃO</Button>
+        </ThemeProvider>
+      </StyledEngineProvider>
+   
   );
 }
 
