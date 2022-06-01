752979b210bde46d6c459e8238ea60131db85b82
Yasmine Melo <yasmine.melo@hotmail.com>
2022-06-01 18:05:48 -0300

    feat: :sparkles: add ImageAvatar componenet

---
+++ src/Components/ImageAvatar/styles.ts
@@ -0,0 +1,21 @@
+import { AppBar, styled } from "@mui/material";
+import theme from '../GlobalStyles/theme';
+
+export const StyledAppBar = styled(AppBar)`
+
+    &.MuiAvatar-circular {
+
+    }
+
+    &.MuiAvatar-rounded {
+
+    }
+
+    &.MuiAvatar-square {
+
+    }
+
+    &.MuiAvatar-colorDefault {
+
+    }
+`;
